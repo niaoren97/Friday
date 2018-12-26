fcfa04785b01b9fa6df231731cfa577b0b605f07
zzdumeng <dumeng8881@gmail.com>
2018-12-26 18:09:54 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import registrator from './plugins/registrator'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+Vue.use(registrator)
 
 new Vue({
   router,
